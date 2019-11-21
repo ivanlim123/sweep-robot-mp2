2adf3533d2a91700f69a034af28e2fcb75dc5379
Ivan Lim <36753941+ivanlim123@users.noreply.github.com>
2019-11-21 14:30:21 +0800

    Update Checking Condition
    Distance, Battery, Filled

--- check.cpp
+++ check.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
@@ -24,6 +25,7 @@ int main() {
     
     int row = 0, col = 0;
     int battery = 0;
+
     MapFile>>row>>col>>battery;
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
@@ -45,25 +47,62 @@ int main() {
     InputFile>>count;
     count++;
     
+    int firstX = 0, firstY = 0;
+    int secondX = 0, secondY = 0;
+    int my_battery = battery;
     for(int i = 0; i < count; i++) {
         InputFile>>x>>y;
-
         if(i==0) {
             bx = x;
             by = y;
+            firstX = x;
+            firstY = y;
         }
+        else {
+            firstX = secondX;
+            firstY = secondY;
+        }
+        secondX = x;
+        secondY = y;
+        
         my_compare[x][y] = '0';
+        if(i!=0) my_battery--;
+        
+        //check distance
+        if((abs(firstX-secondX) + abs(firstY-secondY)!=1) && i!=0) {
+            cout<<"distance is wrong"<<endl;
+            cout<<firstX<<" "<<firstY<<endl;
+            cout<<secondX<<" "<<secondY<<endl;
+        }
+        
+        //check battery
+        if(my_battery<0) {
+            cout<<"low battery"<<endl;
+            cout<<my_battery<<" "<<x<<" "<<y<<endl;
+        }
+        
+        if(x==bx && y==by) {
+            my_battery = battery;
+        }
+        
     }
     my_compare[bx][by] = 'R';
     InputFile.close();
     
+    //check last position
+    if(secondX!=bx && secondY!=by) {
+        cout<<"Not returned"<<endl;
+        cout<<secondX<<" "<<secondY<<endl;
+    }
+    
+    /*
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
             cout<<my_compare[i][j];
         }
         cout<<endl;
     }
-    
+    */
     
     bool same = true;
     for(int i = 0; i < row && same; i++) {
